41ac293923b4261bee0fc292de3b8281eaae2fdf
brother1-4752 <guddlf4752@gmail.com>
2024-01-29 18:34:43 +0900

    feat: 최신화버전

--- src/routes.js
+++ src/routes.js
@@ -4,26 +4,64 @@ const db = require("./doc-db");
 const router = express.Router();
 let filteredDataList = [];
 let columnsList = [];
+let columnFlagList = [];
 
 // 검색 API 구현
 router.post("/search", async (req, res) => {
+  // 검색 조건에 따라 쿼리 생성
+  const createQuery = () => {
+    const query = {};
+
+    if (req.body.userId.length > 0) {
+      query.$or = [
+        {
+          user_id: req.body.userId,
+          customer_user_id: req.body.userId,
+        },
+      ];
+    }
+
+    if (req.body.session_id.length > 0) {
+      query.session_id = req.body.session_id;
+    }
+
+    if (req.body.productName.length > 0) {
+      query.productName = req.body.productName;
+    }
+
+    if (req.body.productNo.length > 0) {
+      query.productNo = req.body.productNo;
+    }
+
+    query.$and = [
+      
+    ]
+
+    return query;
+  };
+
+  const query = createQuery();
+  console.log("쿼리 : ", query);
   const serviceName = req.body.serviceName;
 
   if (columnsList.length > 0) {
     console.log("컬럼 비우기 완료");
     columnsList = [];
+    columnFlagList = [];
   }
 
   const collectionTypes = [
+    // 추후 컬렉션에 데이터 적재 완료시, 아래 코드로 변경
     `${serviceName === "hiver" ? "t_amplitude_h" : "t_amplitude_b"}`,
-    "t_conversions_retargeting_h",
-    "t_installs_h",
+    "t_conversions_retargeting_b",
+    "t_installs_b",
   ];
 
   for (const type of collectionTypes) {
-    const columns = await db.collection(type).findOne();
+    columns = await db.collection(type).findOne();
     console.log(type, "컬럼수", Object.keys(columns).length);
     columnsList.push(...Object.keys(columns));
+    columnFlagList.push(Object.keys(columns)[0]);
   }
 
   if (filteredDataList.length > 0) {
@@ -43,16 +81,15 @@ router.post("/search", async (req, res) => {
   });
 
   const filteredCollectionsNameList = collectionsNameList.filter(
-    (collection) => collection
+    (collection) => collection !== undefined && collection !== "meta_batch_log"
   );
-  const userId = req.body.userId;
 
   try {
     console.log(filteredCollectionsNameList);
 
     console.time("검색 시간");
     for (const collectionName of filteredCollectionsNameList) {
-    // for (const collectionName of ["t_amplitude_h"]) {
+      // for (const collectionName of ["t_inapps_b"]) {
       if (collectionName) {
         const docCounts = await db.collection(collectionName).countDocuments();
 
@@ -69,10 +106,8 @@ router.post("/search", async (req, res) => {
 
         const result = await db
           .collection(collectionName)
-          .find({
-            $or: [{ customer_user_id: userId }, { user_id: userId }],
-          })
-          .sort({ event_time: -1 })
+          .find(query)
+          .sort({ event_time_kst: -1 })
           .toArray();
         filteredDataList.push(...result);
 
@@ -85,7 +120,7 @@ router.post("/search", async (req, res) => {
   }
   console.timeEnd("검색 시간");
 
-  res.json({ dataList: filteredDataList, columnsList });
+  res.json({ dataList: filteredDataList, columnsList, columnFlagList });
 });
 
 module.exports = router;
